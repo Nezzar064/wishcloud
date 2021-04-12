62134ffbe8ded73db9fdf0f3c8970989a692cb0e
Nezzar064 <72224094+Nezzar064@users.noreply.github.com>
2021-04-12 18:16:22 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/models/Role.java
@@ -0,0 +1,25 @@
+package com.example.demo.models;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private long id;
+
+    @Column(name = "role")
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,45 @@
+package com.example.demo.service;
+
+
+import com.example.demo.models.Role;
+import com.example.demo.models.User;
+import com.example.demo.repositories.RoleRepository;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findUserByEmail(long id) {
+        return userRepository.findById(id);
+    }
+
+    public User findUserByUsername(String userName){
+        return userRepository.findByUsername(userName);
+    }
+
+    public User saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(true);
+        Role userRole = roleRepository.findByRole("ADMIN");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
+}
